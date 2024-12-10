c6a240080d68012e2069ed4a232a00c54329e8c1
Pur0idx <peanutpeanut@hotmail.co.th>
2024-12-10 17:55:36 +0700

    handle enter in login pane

--- src/gui/LoginPane.java
+++ src/gui/LoginPane.java
@@ -1,6 +1,7 @@
 package gui;
 
 import components.SoundButton;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.CacheHint;
@@ -8,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -120,6 +122,12 @@ public class LoginPane extends AnchorPane {
                 field.setPromptText("ENTER HERO NAME");
                 field.setStyle(getDefaultFieldStyle());
             }
+        });
+        
+        field.setOnKeyPressed(e -> {
+        	if(e.getCode() == KeyCode.ENTER) {
+        		startGame(field.getText());
+        	}
         });
         
         return field;
