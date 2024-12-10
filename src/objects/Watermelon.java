d73a65cdd76033a691b48e74a01b2f927c652cf5
Pur0idx <peanutpeanut@hotmail.co.th>
2024-12-10 23:27:37 +0700

    watermelon double speed

--- src/objects/Watermelon.java
+++ src/objects/Watermelon.java
@@ -4,16 +4,22 @@ import character.Moodeng;
 import types.FruitType;
 
 public class Watermelon extends BaseFruit {
+    private static final double SPEED_MULTIPLIER = 2;
+    private double originalSpeed;
+	
     public Watermelon() {
         super("Fruitful Recovery", "watermelon.png", 0);
     }
     
     @Override
     public void applyEffect(Moodeng moodeng) {
+        originalSpeed = moodeng.getSpeed();
+        moodeng.setSpeed(originalSpeed * SPEED_MULTIPLIER);
     }
     
     @Override
     public void removeEffect(Moodeng moodeng) {
+        moodeng.setSpeed(originalSpeed);
     }
     
     @Override
